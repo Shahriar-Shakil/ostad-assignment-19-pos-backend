95f58a49372378fd3a83119aa4fcfbfdcdaeef65
shahriar <amivaloasi>
2023-10-18 22:16:00 +0600

    login api added

---
+++ src/app/api/user/login/route.js
@@ -0,0 +1,27 @@
+import { CreateToken } from "@/app/utility/JWTTokenHelper";
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function POST(req, res) {
+  try {
+    const prisma = new PrismaClient();
+    let reqBody = await req.json();
+    const result = await prisma.users.findUnique({ where: reqBody });
+    if (result.length === 0) {
+      return NextResponse.json(
+        { status: "fail", data: result },
+        { status: 401 }
+      );
+    } else {
+      let token = await CreateToken(result["email"], result["id"]);
+      const expirationDate = new Date(Date.now() + 24 * 60 * 60 * 1000);
+      const cookieString = `token=${token}; expires=${expirationDate.toUTCString()}; path="/"`;
+      return NextResponse.json(
+        { status: "success", data: token },
+        { status: 200, headers: { "set-cookie": cookieString } }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json({ status: "fail", data: error });
+  }
+}
