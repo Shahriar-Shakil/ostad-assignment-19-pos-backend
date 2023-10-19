4aa63d9e8469c28baedf07b2eeb1f69df6e14c33
shahriar <amivaloasi>
2023-10-19 21:00:21 +0600

    product meta added

--- src/app/api/dashboard/products/meta/route.js
+++ src/app/api/dashboard/products/meta/route.js
@@ -3,36 +3,40 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 //read products
-export async function GET() {
+export async function GET(req) {
   try {
-    let headerList = headers();
-    let id = headerList.get("id");
+    const headerList = headers();
+    const userId = headerList.get("id");
+    const { searchParams } = new URL(req.url);
+    const product_id = searchParams.get("product_id");
 
     const prisma = new PrismaClient({ log: ["warn", "error"] });
-    const result = await prisma.products.findMany({
-      where: { user_id: parseInt(id) },
+    const result = await prisma.product_meta.findMany({
+      where: { productId: parseInt(product_id), userId: parseInt(userId) },
     });
+
     return NextResponse.json({ status: "success", data: result });
   } catch (error) {
     return NextResponse.json({ status: "fail", data: error });
   }
 }
-//create
 export async function POST(req, res) {
   try {
     const headerList = headers();
     const userId = headerList.get("id");
+
     const reqBody = await req.json();
-    reqBody.user_id = parseInt(userId);
+    reqBody.userId = parseInt(userId);
+
     const prisma = new PrismaClient({ log: ["warn", "error"] });
-    const result = await prisma.products.create({ data: reqBody });
+    const result = await prisma.product_meta.create({ data: reqBody });
     return NextResponse.json({ status: "success", data: result });
   } catch (error) {
     return NextResponse.json({ status: "fail", data: error });
   }
 }
-//update
 
+//update
 export async function PUT(req, res) {
   try {
     const headerList = headers();
@@ -42,10 +46,9 @@ export async function PUT(req, res) {
     const product_id = searchParams.get("product_id");
 
     const reqBody = await req.json();
-
-    const prisma = new PrismaClient({ log: ["error"] });
-    const result = await prisma.products.update({
-      where: { id: parseInt(product_id), user_id: parseInt(userId) },
+    const prisma = new PrismaClient({ log: ["warn", "error"] });
+    const result = await prisma.product_meta.update({
+      where: { productId: parseInt(product_id), userId: parseInt(userId) },
       data: reqBody,
     });
     return NextResponse.json({ status: "success", data: result });
@@ -64,26 +67,8 @@ export async function DELETE(req, res) {
     const product_id = searchParams.get("product_id");
 
     const prisma = new PrismaClient();
-    const result = await prisma.products.delete({
-      where: { id: parseInt(product_id), user_id: parseInt(userId) },
-    });
-    return NextResponse.json({ status: "success", data: result });
-  } catch (error) {
-    return NextResponse.json({ status: "fail", data: error });
-  }
-}
-//single products
-export async function PATCH(req, res) {
-  try {
-    const headerList = headers();
-    const userId = headerList.get("id");
-
-    const { searchParams } = new URL(req.url);
-    const product_id = searchParams.get("product_id");
-
-    const prisma = new PrismaClient();
-    const result = await prisma.products.findUnique({
-      where: { id: parseInt(product_id), user_id: parseInt(userId) },
+    const result = await prisma.product_meta.delete({
+      where: { productId: parseInt(product_id), userId: parseInt(userId) },
     });
     return NextResponse.json({ status: "success", data: result });
   } catch (error) {
